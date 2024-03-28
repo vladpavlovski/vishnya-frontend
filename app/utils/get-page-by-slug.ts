31d94cee21bf85b1df80d58db8db50c6e7033899
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-03-28 18:38:53 +0100

    remove locale support

--- app/utils/get-page-by-slug.ts
+++ app/utils/get-page-by-slug.ts
@@ -1,12 +1,11 @@
 import { fetchAPI } from '@/app/utils/fetch-api';
 
-export async function getPageBySlug(slug: string, lang: string) {
+export async function getPageBySlug(slug: string) {
   const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
   const path = `/pages`;
   const urlParamsObject = {
     filters: { slug },
-    locale: lang,
   };
   const options = { headers: { Authorization: `Bearer ${token}` } };
   return await fetchAPI(path, urlParamsObject, options);
