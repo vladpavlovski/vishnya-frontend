ba6f05233a3dbd6aa9f76b3eb6f777aca1e5efed
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-04-21 12:56:03 +0200

    add error content to logs

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -14,6 +14,6 @@ export default async function CatalogRoute() {
       sectionRenderer(section, index)
     );
   } catch (error: any) {
-    console.error('Missing or invalid credentials');
+    console.error('Missing or invalid credentials:', error);
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,6 @@ export default async function RootRoute() {
       sectionRenderer(section, index)
     );
   } catch (error: any) {
-    console.error('Missing or invalid credentials');
+    console.error('Missing or invalid credentials:', error);
   }
 }
